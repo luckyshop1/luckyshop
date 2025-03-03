c5b6ac168aa135ea1eabe66a4bf16794bad96ef7
luckyshop1 <lordmobile.doducanh@gmail.com>
2025-03-03 10:55:19 +0700

    Add files via upload
    Signed-off-by: luckyshop1 <lordmobile.doducanh@gmail.com>

--- phones.js
+++ phones.js
@@ -1,57 +1,38 @@
-const phones = [{"Tên máy": "Pixel 9 Pro XL Xám", "Cấu hình": 16.256, "Giá": "20.790.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": "Qte"},
- {"Tên máy": "Pixel 7 Pro Xanh", "Cấu hình": 12.128, "Giá": "6.990.000", "Phụ kiện": 0, "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Xperia 1 mark VI Xanh", "Cấu hình": 12.256, "Giá": 25990000, "Phụ kiện": "Fb", "Tình trạng": "Newseal", "Xuất xứ": "VN", "Bảo hành": "BH hãng 24th"},
- {"Tên máy": "Xperia 10 mark VI", "Cấu hình": nan, "Giá": 8990000, "Phụ kiện": "Fb", "Tình trạng": "Newseal", "Xuất xứ": "VN", "Bảo hành": "BH hãng 24th"},
- {"Tên máy": "Xperia 1 mark V", "Cấu hình": 12.256, "Giá": 20490000, "Phụ kiện": "Fb", "Tình trạng": "Newseal", "Xuất xứ": "VN", "Bảo hành": "BH hãng 12th"},
- {"Tên máy": "Xperia 1 mark VI Xanh", "Cấu hình": 12.256, "Giá": "21.490.000", "Phụ kiện": "Fb + Ốp", "Tình trạng": "Keng", "Xuất xứ": "VN", "Bảo hành": "BH T02.2027"},
- {"Tên máy": "Sony Xperia 1 Mark 6 Đen", "Cấu hình": 12.256, "Giá": "22.890.000", "Phụ kiện": "Fb + Ốp", "Tình trạng": 99, "Xuất xứ": "VN", "Bảo hành": "BH T02.2027"},
- {"Tên máy": "X200 Pro Xanh", "Cấu hình": 16.512, "Giá": "18.290.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Đen", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Xanh", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Trắng", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Mini Green", "Cấu hình": 12.256, "Giá": "14.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Mini Hồng", "Cấu hình": 12.256, "Giá": "14.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Mini Hồng", "Cấu hình": 16.512, "Giá": "16.790.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Mini Trắng", "Cấu hình": 16.512, "Giá": "16.790.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Mini Green", "Cấu hình": 16.512, "Giá": "16.790.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Trắng", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Titan", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Blue", "Cấu hình": 12.256, "Giá": "16.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Trắng", "Cấu hình": 16.512, "Giá": "19.290.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Titan", "Cấu hình": 16.512, "Giá": "19.290.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X200 Pro Blue", "Cấu hình": 16.512, "Giá": "19.290.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Mi 14T Đen", "Cấu hình": 12.256, "Giá": "12.790.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "VN", "Bảo hành": "BH T10.2026 TGDD"},
- {"Tên máy": "Find X7 Ultra Đen", "Cấu hình": 16.256, "Giá": "inbox", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Find X8 Pro Xanh", "Cấu hình": 12.256, "Giá": "17.290.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Find X8 Pro Trắng", "Cấu hình": 16.512, "Giá": "18.890.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Find X8 Đen", "Cấu hình": 16.512, "Giá": "19.590.000", "Phụ kiện": "Fb", "Tình trạng": "New Seal", "Xuất xứ": "VN", "Bảo hành": "BH 12 Tháng"},
- {"Tên máy": "Find N2 Flip Tím", "Cấu hình": 8.256, "Giá": "6.890.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "VN", "Bảo hành": "BH ~ 3 Tháng"},
- {"Tên máy": "Find N2 Flip Tím", "Cấu hình": 8.256, "Giá": "6.890.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "VN", "Bảo hành": "BH 6 Tháng"},
- {"Tên máy": "Honor V3 Xanh", "Cấu hình": 12.512, "Giá": "31.290.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": "BH T01.2027"},
- {"Tên máy": "Magic 7 Pro Xám", "Cấu hình": 16.512, "Giá": "18.790.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "OnePlus Ace 5", "Cấu hình": 12.256, "Giá": "8.590.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Motorola 50 Ultra Vàng", "Cấu hình": "16.1Tb", "Giá": "14.290.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "Redmi Note 14 Pro Plus", "Cấu hình": 8.256, "Giá": "7.590.000", "Phụ kiện": "Fb", "Tình trạng": "Like New", "Xuất xứ": "VN", "Bảo hành": "BH tới 13/08/2026"},
- {"Tên máy": "X200 Pro Xám Titan", "Cấu hình": 16.512, "Giá": "18.790.000", "Phụ kiện": "Fb", "Tình trạng": "Keng", "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X100 Ultra Xám", "Cấu hình": 12.256, "Giá": "16.390.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "X100 Ultra Xám", "Cấu hình": 16.512, "Giá": "17.590.000", "Phụ kiện": "Fb", "Tình trạng": 99, "Xuất xứ": "xt", "Bảo hành": nan},
- {"Tên máy": "V Purse Xanh", "Cấu hình": 16.256, "Giá": "13.190.000", "Phụ kiện": 0, "Tình trạng": 98, "Xuất xứ": "xt", "Bảo hành": "Có ốp, màn đẹp"}];
-
-// Render danh sách điện thoại vào bảng trong HTML
-document.addEventListener("DOMContentLoaded", function () {
-    const tableBody = document.getElementById("phone-list");
-    if (!tableBody) return;
-
-    phones.forEach(phone => {
-        const row = `<tr>
-            <td>${phone.name}</td>
-            <td>${phone.config}</td>
-            <td>${phone.price.toLocaleString("vi-VN")} VNĐ</td>
-            <td>${phone.accessories}</td>
-            <td>${phone.condition}</td>
-            <td>${phone.origin}</td>
-            <td>${phone.warranty}</td>
-        </tr>`;
-        tableBody.innerHTML += row;
-    });
-});
+const phones = [
+    {
+        "Tên máy": "Pixel 9 Pro XL Xám",
+        "Cấu hình": "16GB/256GB",
+        "Giá": "20.790.000",
+        "Phụ kiện": "Fb",
+        "Tình trạng": "Keng",
+        "Xuất xứ": "xt",
+        "Bảo hành": "Qte"
+    },
+    {
+        "Tên máy": "Pixel 7 Pro Xanh",
+        "Cấu hình": "12GB/128GB",
+        "Giá": "6.990.000",
+        "Phụ kiện": 0,
+        "Tình trạng": 99,
+        "Xuất xứ": "xt",
+        "Bảo hành": "Không bảo hành"
+    },
+    {
+        "Tên máy": "Xperia 1 mark VI Xanh",
+        "Cấu hình": "12GB/256GB",
+        "Giá": "25.990.000",
+        "Phụ kiện": "Fb",
+        "Tình trạng": "Newseal",
+        "Xuất xứ": "VN",
+        "Bảo hành": "BH hãng 24th"
+    },
+    {
+        "Tên máy": "Xperia 10 mark VI",
+        "Cấu hình": "Không có thông tin",
+        "Giá": "8.990.000",
+        "Phụ kiện": "Fb",
+        "Tình trạng": "Newseal",
+        "Xuất xứ": "VN",
+        "Bảo hành": "BH hãng 24th"
+    }
+];
